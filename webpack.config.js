ed998ecec283c6090fa7b054ea5a693d88021bd1
alexketchum <aketchum2112@gmail.com>
2019-11-02 17:00:22 -0700

    Move .babelrc options to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,18 @@ module.exports = {
                 test: /\.(ts|js)x?$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader"
+                    loader: "babel-loader",
+                    options: {
+                        "presets": [
+                            "@babel/preset-env",
+                            "@babel/preset-react",
+                            "@babel/preset-typescript"
+                        ],
+                        "plugins": [
+                            "@babel/proposal-class-properties",
+                            "@babel/proposal-object-rest-spread"
+                        ]
+                    }
                 },
             },
             {
